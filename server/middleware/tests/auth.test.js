b24c70c3c148d75b501ddbbb802e8f0bb179df25
DevelopJKong <ybaram40@gmail.com>
2022-03-05 15:50:08 +0900

    7.3 Auth 미들웨어 더 작성하기 💡 (실패) 완료

---
+++ server/middleware/tests/auth.test.js
@@ -0,0 +1,118 @@
+import httpMocks from "node-mocks-http";
+import { isAuth } from "../auth.js";
+import faker from "faker";
+import jwt from "jsonwebtoken";
+import * as userRepository from "../../data/auth.js";
+
+jest.mock("jsonwebtoken");
+jest.mock("../../data/auth.js");
+
+describe("Auth Middleware", () => {
+  it("returns 401 for the request without Authorization header", async () => {
+    //given
+    const request = httpMocks.createRequest({
+      method: "GET",
+      url: "/tweets",
+    });
+    const response = httpMocks.createResponse();
+    const next = jest.fn();
+
+    //when
+    await isAuth(request, response, next);
+
+    //then
+    expect(response.statusCode).toBe(401);
+    expect(response._getJSONData().message).toBe("Authentication Error");
+    expect(next).not.toBeCalled();
+  });
+
+  it("returns 401 for the request with unsupported Authorization header", async () => {
+    //given
+    const request = httpMocks.createRequest({
+      method: "GET",
+      url: "/tweets",
+      headers: { Authorization: "Basic" },
+    });
+    const response = httpMocks.createResponse();
+    const next = jest.fn();
+
+    //when
+    await isAuth(request, response, next);
+
+    //then
+    expect(response.statusCode).toBe(401);
+    expect(response._getJSONData().message).toBe("Authentication Error");
+    expect(next).not.toBeCalled();
+  });
+
+  it("returns 401 for the request with invalid JWT", async () => {
+    //given
+    const token = faker.random.alphaNumeric(128);
+    const request = httpMocks.createRequest({
+      method: "GET",
+      url: "/tweets",
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    const response = httpMocks.createResponse();
+    const next = jest.fn();
+    jwt.verify = jest.fn((token, secret, callback) => {
+      callback(new Error("bad token"), undefined);
+    });
+
+    //when
+    await isAuth(request, response, next);
+
+    //then
+    expect(response.statusCode).toBe(401);
+    expect(response._getJSONData().message).toBe("Authentication Error");
+    expect(next).not.toBeCalled();
+  });
+
+  it("returns 401 when cannot find a user by id from the JWT", async () => {
+    //given
+    const token = faker.random.alphaNumeric(128);
+    const userId = faker.random.alphaNumeric(32);
+    const request = httpMocks.createRequest({
+      method: "GET",
+      url: "/tweets",
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    const response = httpMocks.createResponse();
+    const next = jest.fn();
+    jwt.verify = jest.fn((token, secret, callback) => {
+      callback(undefined, { id: userId });
+    });
+
+    userRepository.findById = jest.fn((id) => Promise.resolve(undefined));
+
+    //when
+    await isAuth(request, response, next);
+
+    //then
+    expect(response.statusCode).toBe(401);
+    expect(response._getJSONData().message).toBe("Authentication Error");
+    expect(next).not.toBeCalled();
+  });
+  it("returns 401 for the request with invalid JWT", async () => {
+    //given
+    const token = faker.random.alphaNumeric(128);
+    const request = httpMocks.createRequest({
+      method: "GET",
+      url: "/tweets",
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    const response = httpMocks.createResponse();
+    const next = jest.fn();
+    jwt.verify = jest.fn((token, secret, callback) => {
+      callback(new Error("bad token"), undefined);
+    });
+
+    //when
+    await isAuth(request, response, next);
+
+    //then
+    expect(response.statusCode).toBe(401);
+    expect(response._getJSONData().message).toBe("Authentication Error");
+    expect(next).not.toBeCalled();
+  });
+});
