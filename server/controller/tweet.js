6af959b3734dd920e83ab48b6735152527bf37e6
DevelopJKong <ybaram40@gmail.com>
2022-03-11 12:05:11 +0900

    8.2 통합테스트 환경 설정 완료

--- server/controller/tweet.js
+++ server/controller/tweet.js
@@ -3,22 +3,15 @@ export class TweetController {
     this.tweets = tweetRepository;
     this.getSocket = getSocket;
   }
-
-  /*
-    arrow function으로 변경하면 자동으로 this가 바인딩이 된다
-    function 과 arrow의 function에서 this의 차이점은 다르다는것은
-    알고있지만 정확하게 어떻게 다른지 설명을 할수있어야 겠다
-  */
-
-
-  getTweets =  async(req, res) => {
+  getTweets = async (req, res) => {
     const username = req.query.username;
     const data = await (username
       ? this.tweets.getAllByUsername(username)
       : this.tweets.getAll());
     res.status(200).json(data);
-  }
-  getTweet = (req, res, next) =>  {
+  };
+
+  getTweet = async (req, res, next) => {
     const id = req.params.id;
     const tweet = await this.tweets.getById(id);
     if (tweet) {
@@ -26,14 +19,16 @@ export class TweetController {
     } else {
       res.status(404).json({ message: `Tweet id(${id}) not found` });
     }
-  }
-  createTweet = (req, res, next) => {
+  };
+
+  createTweet = async (req, res, next) => {
     const { text } = req.body;
     const tweet = await this.tweets.create(text, req.userId);
     res.status(201).json(tweet);
-    this.getSocket.emit("tweets", tweet);
-  }
- updateTweet = (req, res, next) => {
+    this.getSocket().emit('tweets', tweet);
+  };
+
+  updateTweet = async (req, res, next) => {
     const id = req.params.id;
     const text = req.body.text;
     const tweet = await this.tweets.getById(id);
@@ -45,8 +40,9 @@ export class TweetController {
     }
     const updated = await this.tweets.update(id, text);
     res.status(200).json(updated);
-  }
-  deleteTweet = (req, res, next) => {
+  };
+
+  deleteTweet = async (req, res, next) => {
     const id = req.params.id;
     const tweet = await this.tweets.getById(id);
     if (!tweet) {
@@ -57,5 +53,5 @@ export class TweetController {
     }
     await this.tweets.remove(id);
     res.sendStatus(204);
-  }
+  };
 }
