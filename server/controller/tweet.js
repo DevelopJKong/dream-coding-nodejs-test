70345fcf8799601c758d4826df46e250d489c740
DevelopJKong <ybaram40@gmail.com>
2022-03-11 11:16:32 +0900

    7.10트윗 컨트롤러 테스트 하기💡 (파트1) 완료

--- server/controller/tweet.js
+++ server/controller/tweet.js
@@ -1,54 +1,61 @@
-import * as tweetRepository from '../data/tweet.js';
-import { getSocketIO } from '../connection/socket.js';
-
-export async function getTweets(req, res) {
-  const username = req.query.username;
-  const data = await (username
-    ? tweetRepository.getAllByUsername(username)
-    : tweetRepository.getAll());
-  res.status(200).json(data);
-}
-
-export async function getTweet(req, res, next) {
-  const id = req.params.id;
-  const tweet = await tweetRepository.getById(id);
-  if (tweet) {
-    res.status(200).json(tweet);
-  } else {
-    res.status(404).json({ message: `Tweet id(${id}) not found` });
+export class TweetController {
+  constructor(tweetRepository, getSocket) {
+    this.tweets = tweetRepository;
+    this.getSocket = getSocket;
   }
-}
 
-export async function createTweet(req, res, next) {
-  const { text } = req.body;
-  const tweet = await tweetRepository.create(text, req.userId);
-  res.status(201).json(tweet);
-  getSocketIO().emit('tweets', tweet);
-}
+  /*
+    arrow function으로 변경하면 자동으로 this가 바인딩이 된다
+    function 과 arrow의 function에서 this의 차이점은 다르다는것은
+    알고있지만 정확하게 어떻게 다른지 설명을 할수있어야 겠다
+  */
 
-export async function updateTweet(req, res, next) {
-  const id = req.params.id;
-  const text = req.body.text;
-  const tweet = await tweetRepository.getById(id);
-  if (!tweet) {
-    return res.status(404).json({ message: `Tweet not found: ${id}` });
-  }
-  if (tweet.userId !== req.userId) {
-    return res.sendStatus(403);
-  }
-  const updated = await tweetRepository.update(id, text);
-  res.status(200).json(updated);
-}
 
-export async function deleteTweet(req, res, next) {
-  const id = req.params.id;
-  const tweet = await tweetRepository.getById(id);
-  if (!tweet) {
-    return res.status(404).json({ message: `Tweet not found: ${id}` });
+  getTweets =  async(req, res) => {
+    const username = req.query.username;
+    const data = await (username
+      ? this.tweets.getAllByUsername(username)
+      : this.tweets.getAll());
+    res.status(200).json(data);
   }
-  if (tweet.userId !== req.userId) {
-    return res.sendStatus(403);
+  getTweet = (req, res, next) =>  {
+    const id = req.params.id;
+    const tweet = await this.tweets.getById(id);
+    if (tweet) {
+      res.status(200).json(tweet);
+    } else {
+      res.status(404).json({ message: `Tweet id(${id}) not found` });
+    }
   }
-  await tweetRepository.remove(id);
-  res.sendStatus(204);
+  createTweet = (req, res, next) => {
+    const { text } = req.body;
+    const tweet = await this.tweets.create(text, req.userId);
+    res.status(201).json(tweet);
+    this.getSocket.emit("tweets", tweet);
+  }
+ updateTweet = (req, res, next) => {
+    const id = req.params.id;
+    const text = req.body.text;
+    const tweet = await this.tweets.getById(id);
+    if (!tweet) {
+      return res.status(404).json({ message: `Tweet not found: ${id}` });
+    }
+    if (tweet.userId !== req.userId) {
+      return res.sendStatus(403);
+    }
+    const updated = await this.tweets.update(id, text);
+    res.status(200).json(updated);
+  }
+  deleteTweet = (req, res, next) => {
+    const id = req.params.id;
+    const tweet = await this.tweets.getById(id);
+    if (!tweet) {
+      return res.status(404).json({ message: `Tweet not found: ${id}` });
+    }
+    if (tweet.userId !== req.userId) {
+      return res.sendStatus(403);
+    }
+    await this.tweets.remove(id);
+    res.sendStatus(204);
+  }
 }
